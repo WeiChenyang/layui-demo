ed1835c42bf7e5f8b6c6c8889d98b7fcfa214f75
weichenyang <weichenyang@cnbmtech.com>
2019-11-12 10:27:46 +0800

    初始化

---
+++ datas/nav.js
@@ -0,0 +1,77 @@
+var navs = [{
+	"title": "一级标题01",
+	"icon": "fa-cubes",
+	"spread": true,
+	"children": [{
+		"title": "二级标题01",
+		"icon": "&#xe641;",
+		"href": "./main.html"
+	}, {
+		"title": "二级标题02",
+		"icon": "&#xe63c;",
+		"href": "./main.html"
+	}, {
+		"title": "二级标题03",
+		"icon": "&#xe63c;",
+		"href": "./main.html"
+	}]
+}, {
+	"title": "一级标题02",
+	"icon": "fa-cogs",
+	"spread": false,
+	"children": [{
+		"title": "二级标题04",
+		"icon": "fa-table",
+		"href": "./main.html"
+	}, {
+		"title": "二级标题05",
+		"icon": "fa-navicon",
+		"href": "./main.html"
+	}]
+}, {
+	"title": "一级标题03",
+	"icon": "&#x1002;",
+	"spread": false,
+	"children": [{
+		"title": "iCheck组件",
+		"icon": "fa-check-square-o",
+		"href": "./main.html"
+	}]
+}, {
+	"title": "地址本",
+	"icon": "fa-address-book",
+	"href": "",
+	"spread": false,
+	"children": [{
+		"title": "Github",
+		"icon": "fa-github",
+		"href": "https://www.github.com/"
+	}, {
+		"title": "QQ",
+		"icon": "fa-qq",
+		"href": "http://www.qq.com/"
+	}, {
+		"title": "Fly社区",
+		"icon": "&#xe609;",
+		"href": "http://fly.layui.com/"
+	}, {
+		"title": "新浪微博",
+		"icon": "fa-weibo",
+		"href": "http://weibo.com/"
+	}]
+}, {
+	"title": "一级导航",
+	"icon": "fa-stop-circle",
+	"href": "https://www.baidu.com",
+	"spread": false
+}, {
+	"title": "其他",
+	"icon": "fa-stop-circle",
+	"href": "#",
+	"spread": false,
+	"children": [{
+		"title": "子窗体中打开选项卡",
+		"icon": "fa-github",
+		"href": "cop.html"
+	}]
+}];
